cd241aa8d234e47dfd2eb30dc3aa29191c8815bd
Aurelio Borges <aurelioaborges@gmail.com>
2023-10-19 16:42:58 -0300

    FS-09: create AddToCartButton component

---
+++ src/components/AddToCartButton.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import { IoIosCart } from "react-icons/io";
+
+export default function AddToCartButton() {
+  return (
+    <Link
+      className="flex justify-center text-xl lg:text-2xl xl:text-3xl 2xl:text-5xl"
+      href={"/cart"}
+    >
+      <button className="flex w-full max-w-sm items-center justify-center gap-2 rounded-full bg-gray-950 p-5 text-white dark:bg-gray-50 dark:text-black md:max-w-full lg:py-8 xl:py-10 2xl:py-12">
+        Add to cart
+        <span>
+          <IoIosCart />
+        </span>
+      </button>
+    </Link>
+  );
+}
