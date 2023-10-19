59afdee97dea1b817a256cc38958f79b5a2ded62
Aurelio Borges <aurelioaborges@gmail.com>
2023-10-19 16:39:30 -0300

    FS-03 config: Next Themes setup

---
+++ src/utils/Providers.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import Loading from "@/components/Loading";
+import { ThemeProvider } from "next-themes";
+import { useEffect, useState } from "react";
+
+export default function Providers({ children }: any) {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return <Loading />;
+  }
+
+  return <ThemeProvider attribute="class">{children}</ThemeProvider>;
+}
