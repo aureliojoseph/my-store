7acaafc384e759e59569976f3f21725e3d80e7a4
Aurelio Borges <aurelioaborges@gmail.com>
2023-10-20 15:41:25 -0300

    FS-09 fix: add styles for hover state

--- src/components/AddToCartButton.tsx
+++ src/components/AddToCartButton.tsx
@@ -7,7 +7,7 @@ export default function AddToCartButton() {
       className="flex justify-center text-xl lg:text-2xl xl:text-3xl 2xl:text-5xl"
       href={"/cart"}
     >
-      <button className="flex w-full max-w-sm items-center justify-center gap-2 rounded-full bg-gray-950 p-5 text-white dark:bg-gray-50 dark:text-black md:max-w-full lg:py-8 xl:py-10 2xl:py-12">
+      <button className="flex w-full max-w-sm items-center justify-center gap-2 rounded-full bg-gray-950 p-5 text-white transition-all duration-300 ease-linear hover:bg-gray-800 dark:bg-gray-50 dark:text-black dark:hover:bg-gray-300 md:max-w-full lg:py-8 xl:py-10 2xl:py-12">
         Add to cart
         <span>
           <IoIosCart />
