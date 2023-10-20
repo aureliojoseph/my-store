d81c6ab38b536a477441e875135ef506ca2f2403
Aurelio Borges <aurelioaborges@gmail.com>
2023-10-20 15:40:22 -0300

    FS-04: add link to About page

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -13,17 +13,24 @@ export default function NavBar() {
         <IoMdHome className="cursor-pointer rounded-full p-1 text-4xl transition-all duration-300 ease-linear hover:bg-gray-300 dark:hover:bg-gray-400" />
       </Link>
 
-      {theme === "dark" ? (
-        <IoIosSunny
-          className="cursor-pointer rounded-full p-1 text-4xl transition-all duration-300 ease-linear dark:hover:bg-gray-400"
-          onClick={() => setTheme("light")}
-        />
-      ) : (
-        <IoIosMoon
-          className="cursor-pointer rounded-full p-1 text-4xl transition-all duration-300 ease-linear hover:bg-gray-300"
-          onClick={() => setTheme("dark")}
-        />
-      )}
+      <div className="flex items-center gap-4">
+        <Link href={"/about"}>
+          <p className="cursor-pointer rounded-full p-2 text-base transition-all duration-300 ease-linear hover:bg-gray-300 dark:hover:bg-gray-400">
+            About
+          </p>
+        </Link>
+        {theme === "dark" ? (
+          <IoIosSunny
+            className="cursor-pointer rounded-full p-1 text-4xl transition-all duration-300 ease-linear dark:hover:bg-gray-400"
+            onClick={() => setTheme("light")}
+          />
+        ) : (
+          <IoIosMoon
+            className="cursor-pointer rounded-full p-1 text-4xl transition-all duration-300 ease-linear hover:bg-gray-300"
+            onClick={() => setTheme("dark")}
+          />
+        )}
+      </div>
     </nav>
   );
 }
